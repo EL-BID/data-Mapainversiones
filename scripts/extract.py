60e0acde2a825d86eff94f6e28022a0206c405b6
MZhiyenbayev <meiran1991@gmail.com>
2024-06-21 18:52:16 +0300

    [data][l]: added data package and toc file

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,13 +1,76 @@
 from dataflows import Flow, load, dump_to_path, add_metadata,update_resource
+from azure.storage.blob import BlobServiceClient, ContainerClient, BlobClient
+import boto3
+from botocore.exceptions import NoCredentialsError,ClientError
+import os
+from dotenv import load_dotenv
 
-def dataset1_process():
+# Load environment variables from .env file
+load_dotenv()
+
+def package_init():
     flow = Flow(
-        load("datasets/dataset-1/data/BID_Base_Cla_end_mip.csv", format='csv', name="bid_base_cla_end_mip"),
-        update_resource('bid_base_cla_end_mip', path='data/bid_base_cla_end_mip.csv'),
-        add_metadata(name='bid_base_cla_end_mip', title='bid_base_cla_end_mip', country='Dominican Republic'),
-        #dump_to_path(out_path="datasets/dataset-1/")
+        load("datasets/PRY/PROYECTOS/OPENDATA_PROYECTOS.csv", format='csv', name="opendata-proyectos"),
+        dump_to_path(out_path="datasets/PRY/PROYECTOS/")
     )
-    flow.process()
-    
+    flow.process()   
+               
+                     
+def copy_azure_to_r2(container_name, blob_name, blob_content):
+    try:
+        # Prepend the Azure container name to the blob name
+        r2_blob_name = f"{container_name}/{blob_name}"
+        
+        # Check if the object already exists in the R2 bucket
+        try:
+            r2_client.head_object(Bucket=r2_bucket_name, Key=r2_blob_name)
+            print(f"Skipping {r2_blob_name}: already exists in R2 bucket.")
+        except ClientError as e:
+            if e.response['Error']['Code'] == '404':
+                # Object does not exist, proceed with upload
+                r2_client.put_object(Bucket=r2_bucket_name, Key=r2_blob_name, Body=blob_content)
+                print(f"Successfully copied {r2_blob_name} to R2 in {r2_location}.")
+            else:
+                # Handle other potential errors
+                print(f"Error checking if {r2_blob_name} exists: {e}")
+    except NoCredentialsError:
+        print("Credentials not available.")
+    except Exception as e:
+        print(f"Failed to copy {blob_name}: {e}")
+        
+def full_load():
+    # Azure Storage credentials
+    azure_storage_account_name = os.getenv('AZURE_STORAGE_ACCOUNT_NAME')
+    azure_storage_account_key = os.getenv('AZURE_STORAGE_ACCOUNT_KEY')
+    azure_container_name = os.getenv('AZURE_CONTAINER_NAME')
+    # Cloudflare R2 credentials
+    r2_access_key_id = os.getenv('R2_ACCESS_KEY_ID')
+    r2_secret_access_key = os.getenv('R2_SECRET_ACCESS_KEY')
+    r2_bucket_name = os.getenv('R2_BUCKET_NAME')
+    r2_endpoint_url = os.getenv('R2_ENDPOINT_URL')
+    r2_location = os.getenv('R2_LOCATION')
+
+    # Initialize Azure Blob Service Client
+    azure_connection_string = f"DefaultEndpointsProtocol=https;AccountName={azure_storage_account_name};AccountKey={azure_storage_account_key};EndpointSuffix=core.windows.net"
+    azure_blob_service_client = BlobServiceClient.from_connection_string(azure_connection_string)
+    azure_container_client = azure_blob_service_client.get_container_client(azure_container_name)
+
+
+    # Initialize Cloudflare R2 client using boto3
+    r2_client = boto3.client('s3', endpoint_url=r2_endpoint_url,
+                             aws_access_key_id=r2_access_key_id,
+                             aws_secret_access_key=r2_secret_access_key,
+                             region_name=r2_location)
+    # List and copy blobs
+    for blob in azure_container_client.list_blobs():
+        blob_client = azure_container_client.get_blob_client(blob)
+        blob_properties = blob_client.get_blob_properties()
+        if blob_properties.size > 0:
+            blob_content = blob_client.download_blob().readall()
+            copy_azure_to_r2(azure_container_name,blob.name, blob_content)
+        else:
+            print(f"Skipping empty blob: {blob.name}")
+
+
 if __name__ == '__main__':
-    dataset1_process()
+    full_load()
---
+++ scripts/process.py
@@ -0,0 +1,92 @@
+import boto3
+from botocore.exceptions import NoCredentialsError,ClientError
+
+import csv
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+def add_to_toc(blob_name, toc_file):
+    '''
+    blob sample: opendata/PRY/MAPAINVDB/PROYECTOS/CSV/2024/06/10/OPENDATA_PROYECTOS.CSV
+    resource_name sample:  OPENDATA_PROYECTOS.csv
+    toc_file sample: toc_+resource_name
+    '''
+    domain="https://air.portaljs.com/"
+    blob_parts = blob_name.split('/')
+    date = '-'.join(blob_parts[5:8])
+    uri = domain+blob_name
+    file_exists = os.path.isfile(toc_file)
+    with open(toc_file, mode='a', newline='') as file:
+        fieldnames = ['date', 'uri']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        if not file_exists:
+            writer.writeheader()
+        writer.writerow({'date': date, 'uri': uri})
+    print(f"Data appended to {toc_file}")
+#add_to_toc("opendata/PRY/MAPAINVDB/PROYECTOS/CSV/2024/06/10/OPENDATA_PROYECTOS.CSV", "toc_OPENDATA_PROYECTOS.csv")
+
+
+
+def toc_file_init(prefix):
+    r2_access_key_id = os.getenv('R2_ACCESS_KEY_ID')
+    r2_secret_access_key = os.getenv('R2_SECRET_ACCESS_KEY')
+    r2_bucket_name = os.getenv('R2_BUCKET_NAME')
+    r2_endpoint_url = os.getenv('R2_ENDPOINT_URL')
+    r2_location = os.getenv('R2_LOCATION')
+    r2_client = boto3.client(
+        's3',
+        endpoint_url=r2_endpoint_url,
+        aws_access_key_id=r2_access_key_id,
+        aws_secret_access_key=r2_secret_access_key,
+        region_name=r2_location  # Setting the region/location
+    )
+    try:
+        response = r2_client.list_objects_v2(Bucket=r2_bucket_name, Prefix=prefix)
+        objects = response.get('Contents', [])
+        if not objects:
+            print(f"No objects found in R2 bucket {r2_bucket_name} with prefix {prefix}.")
+        else:
+            for obj in objects:
+                blob_name = obj['Key']
+                toc_file = "toc_"+obj['Key'].split('/')[-1]
+                add_to_toc(blob_name, toc_file)
+                #print(f"Found object: {obj['Key']} (Size: {obj['Size']} bytes)")
+    except NoCredentialsError:
+        print("Credentials not available.")
+    except Exception as e:
+        print(f"Failed to list objects in R2 bucket: {e}")
+
+#specific_prefix = "opendata/PRY/MAPAINVDB/PROYECTOS/CSV/"  # Replace with the desired prefix
+#toc_file_init(specific_prefix)
+
+def upload_file_to_r2(file_path, key):
+    # Cloudflare R2 credentials
+    r2_access_key_id = os.getenv('R2_ACCESS_KEY_ID')
+    r2_secret_access_key = os.getenv('R2_SECRET_ACCESS_KEY')
+    r2_bucket_name = os.getenv('R2_BUCKET_NAME')
+    r2_endpoint_url = os.getenv('R2_ENDPOINT_URL')
+    r2_location = os.getenv('R2_LOCATION')
+    
+    r2_client = boto3.client('s3', endpoint_url=r2_endpoint_url,
+                             aws_access_key_id=r2_access_key_id,
+                             aws_secret_access_key=r2_secret_access_key,
+                             region_name=r2_location)
+    try:
+        with open(file_path, 'rb') as file:
+            r2_client.put_object(Bucket=r2_bucket_name, Key=key, Body=file)
+            print(f"Successfully uploaded {file_path} to R2 with key {key}.")
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+    except NoCredentialsError:
+        print("Credentials not available.")
+    except Exception as e:
+        print(f"Failed to upload {file_path}: {e}")
+        
+        
+#local_csv_path = "toc_OPENDATA_PROYECTOS.csv"
+#pload_key = "opendata/PRY/MAPAINVDB/PROYECTOS/toc_OPENDATA_PROYECTOS.csv"
+upload_file_to_r2(local_csv_path, upload_key)
+        
